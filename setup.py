18dfa76916ebf707061de4f52d1b41a5b181d573
tejas patil <patiltejas12051@gmail.com>
2023-07-08 23:23:02 +0530

    added

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+VERSION = '0.0.1'
+PROJECT_NAME = 'Vehicle Number Plate Detection'
+AUTHOR_NAME = 'Tejas Patil'
+DESCRIPTION = ' This is a Vehicle Number Plate Detection'
+
+setuptools.setup(
+    version=VERSION,
+    PROJECT_NAME = PROJECT_NAME,
+    author= AUTHOR_NAME,
+    description= DESCRIPTION,
+    package_dir= {"":"src"},
+    packages= setuptools.find_packages(where='src')
+)
